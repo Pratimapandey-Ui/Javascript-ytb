8678d3bae36c5a891f3ba73f001199b7c097ff87
Pratimapandey-Ui <pratimapandey1822@gmail.com>
2026-04-04 09:02:56 +0000

    learning datatypes

---
+++ Basics/01_database.js
@@ -0,0 +1,18 @@
+"use strict";  //treat all JS code as newer version
+//alert(3+3)// we are using node js,not browswer,in browser it pops up the result
+
+console.log(3+3) 
+//datatypes:
+//number
+//bigint
+//string=>""
+//boolean 
+//null=>stand alone value
+//undefined
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object--->imp for interview
+console.log(typeof 123) // number
+console.log(typeof 123n) // bigint
+console.log(typeof "abc") // string
+console.log(typeof true) // boolean
