3ba730a6f806ad05631f3fddca56b46c104d27a6
Pratimapandey-Ui <pratimapandey1822@gmail.com>
2026-06-21 23:51:21 +0530

    Add JavaScript examples for type coercion and comparison
    ddkodkd2d

---
+++ Basics/03.js
@@ -0,0 +1,22 @@
+console.log("1"+2);
+console.log(1+"2");
+console.log("2"+1+1);     //since started with string so it treated like a string
+console.log(2+2+"1");      //didnt started with starting so it addedv first nd then string is used 
+//use brackets only dont play like that
+
+let gameCounter=100;
+gameCounter++;
+++gameCounter;  //both r different
+
+console.log(null>0);   //false
+console.log(null==0);   //fa;se
+console.log(null>=0);   //true--comparisons convert to no and then compare
+
+console.log(undefined==0);  //false
+console.log(undefined<0);  //false
+console.log(undefined>0);  //false--converting undefined to number gives NaN so all false
+
+console.log("2"===2)
+
+
+
